3776e72301283be6d6e3083c5c6a4780c38c5f53
Kirill <>
2019-06-05 03:34:32 +0300

    add transitions between states

--- BlaBlaCar/scene.cpp
+++ BlaBlaCar/scene.cpp
@@ -21,13 +21,14 @@ QGraphicsScene* Scene::drawScene() {
     int dy = 250;
     int currentCarId = ui->chooseCarComboBox->currentIndex() + 1;
     Car<void> currentCar = stateMachine->find_car_by_id(currentCarId);
-    int passengersCount = currentCar.get_passengers()->size();
 
+    QMap<int, QPoint> superStateToPointMap;
     int currentI = 0;
     for(auto stateIterator = stateMachine->get_states_vector()->begin(); stateIterator != stateMachine->get_states_vector()->end(); ++stateIterator) {
 
         QPoint currentStateCenter = QPoint(sceneCenter.x() + sin(2 * M_PI / superStateCount * currentI) * sceneRadius,
                                            sceneCenter.y() + cos(2 * M_PI / superStateCount * currentI) * sceneRadius);
+        superStateToPointMap.insert(stateIterator->get_id(), currentStateCenter);
         if (stateIterator->get_states()->size() != 0) {
             int x0 = currentStateCenter.x() - dx / 2;
             int y0 = currentStateCenter.y() - dy / 2;
@@ -37,15 +38,18 @@ QGraphicsScene* Scene::drawScene() {
                 int currentJ = 1;
                 for (auto passengerIterator = currentCar.get_passengers()->begin(); passengerIterator != currentCar.get_passengers()->end(); ++passengerIterator) {
                     int currentK = 0;
+                    QMap<int, QPoint> subStateToPointMap;
                     int subStateStatesCount = stateIterator->get_states()->size();
                     for(auto substatesIterator = stateIterator->get_states()->begin(); substatesIterator != stateIterator->get_states()->end(); ++substatesIterator) {
                         QPoint currentSubStateCenter = QPoint(points[currentJ].x() + sin(2 * M_PI / subStateStatesCount * currentK) * dx / 6,
                                                       points[currentJ].y() + cos(2 * M_PI / subStateStatesCount * currentK) * dy / 6);
+                        subStateToPointMap.insert(substatesIterator->get_id(), currentSubStateCenter);
                         drawState(scene, currentSubStateCenter, subStateRadius, QString::fromStdString(substatesIterator->get_value()), passengerIterator->get_state_id() == substatesIterator->get_id()); // look at this
                         currentK++;
                     }
                     drawText(scene, points[currentJ] - QPoint(0, dy / 4), QString::fromStdString(passengerIterator->get_login()));
                     currentJ++;
+                    drawTransitions(scene, stateMachine, subStateToPointMap);
                 }
             } else {
                 drawSuperState(scene, x0, y0, dx, dy, QString::fromStdString(stateIterator->get_value()), false);
@@ -56,6 +60,7 @@ QGraphicsScene* Scene::drawScene() {
         }
         currentI++;
     }
+    drawTransitions(scene, stateMachine, superStateToPointMap);
     return scene;
 }
 
@@ -98,4 +103,34 @@ QPoint* Scene::getQuarterCenters(int x0, int y0, int dx, int dy) {
     QPoint quarterCenter3 = QPoint(x0 + dx / 4, y0 + 3 * dy / 4);
     QPoint quarterCenter4 = QPoint(x0 + 3 * dx / 4, y0 + 3 * dy / 4);
     return new QPoint[4]{quarterCenter1, quarterCenter2, quarterCenter3, quarterCenter4};
+}
+
+void Scene::drawTransitions(QGraphicsScene *scene, StateMachine<std::string, Allocator<std::string> > *, QMap<int, QPoint> map) {
+    for(auto transitionIterator = stateMachine->get_transitions_vector()->begin(); transitionIterator != stateMachine->get_transitions_vector()->end(); ++transitionIterator) {
+        QPoint from = map.value(transitionIterator->get_initial_state().get_id());
+        QPoint to = map.value(transitionIterator->get_final_state().get_id());
+        scene->addLine(QLineF(from, to));
+        scene->addPolygon(getArrowForTransition(from, to), QPen(), QBrush(QColor("black")));
+    }
+}
+
+QPolygonF Scene::getArrowForTransition(QPointF from, QPointF to) {
+    int head_length = 10;
+    int head_width = 10;
+    float dx = to.x() - from.x();
+    float dy = to.y() - from.y();
+    float length = std::sqrt(dx*dx + dy*dy);
+
+    const auto ux = dx / length;
+    const auto uy = dy / length;
+
+    const auto vx = -uy;
+    const auto vy = ux;
+
+    const auto half_width = 0.5f * head_width;
+    QPolygonF polygon = QPolygonF();
+    polygon << to
+            << QPointF(to.x() - head_length*ux + half_width*vx, to.y() - head_length*uy + half_width*vy)
+            << QPointF(to.x() - head_length*ux - half_width*vx, to.y() - head_length*uy - half_width*vy);
+    return polygon;
 }
--- BlaBlaCar/scene.h
+++ BlaBlaCar/scene.h
@@ -7,6 +7,8 @@
 #include <QString>
 #include "statemachine.h"
 #include "ui_mainwindow.h"
+#include <QMap>
+#include <QPolygonF>
 
 namespace StateMachineBlaBlaCar
 {
@@ -23,7 +25,9 @@ private:
     void drawEllipse(QGraphicsScene* scene, QPoint center, int radius, bool isActive);
     void drawState(QGraphicsScene* scene, QPoint center, int radius, QString text, bool isActive);
     void drawSuperState(QGraphicsScene* scene, int x0, int y0, int dx, int dy, QString text, bool isActive);
+    void drawTransitions(QGraphicsScene* scene, StateMachine<std::string, Allocator<std::string>>*, QMap<int, QPoint> map);
     QPoint* getQuarterCenters(int x0, int y0, int dx, int dy);
+    QPolygonF getArrowForTransition(QPointF from, QPointF to);
 };
 }
 #endif // SCENE_H
